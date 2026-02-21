f75f58f25cc0372db38870568b210b3f21588179
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-02-21 21:16:11 +0530

    Update Instructor.java

--- SpringAnnotationDemo/src/com/config/annotations/Instructor.java
+++ SpringAnnotationDemo/src/com/config/annotations/Instructor.java
@@ -11,11 +11,22 @@ import org.springframework.stereotype.Component;
 @Component("inst")
 @Scope("prototype")
 public class Instructor {
-
-	@Value("101")
+//For Integer
+	//@Value("101")
+	//@Value("#{4+2}")
+	//@Value("#{T(java.lang.Integer).MIN_VALUE}")
+	//@Value("#{new Integer(88)}")
+	@Value("#{T(java.lang.Math).abs(-99)}")
 	private int id;
 
-	@Value("Dhiraj")
+
+//For String
+	//@Value("Dhiraj")
+	//@Value("#{'Dhiraj Waghmare'}")
+	//@Value("#{new java.lang.String('Dhiraj Waghmare')}")
+	//@Value("#{'Dhiraj Waghmare'.toUpperCase()}")
+	@Value("#{'Dhiraj Waghmare'.toLowerCase()}")
+
 	private String name;
 
 	@Value("#{topics}")
@@ -23,6 +34,10 @@ public class Instructor {
 
 	@Autowired
 	private Profile Profile;
+	
+	//@Value("#{(2+4>5)}")
+	@Value("#{(2+4>8?false:true)}")
+	private boolean active;
 
 	public int getId() {
 		return id;
@@ -42,9 +57,11 @@ public class Instructor {
 
 	@Override
 	public String toString() {
-		return "Instructor [id=" + id + ", name=" + name + ", topics=" + topics + ", Profile=" + Profile + "]";
+		return "Instructor [id=" + id + ", name=" + name + ", topics=" + topics + ", Profile=" + Profile + ", active="
+				+ active + "]";
 	}
 
+	
 	
 	
 }
