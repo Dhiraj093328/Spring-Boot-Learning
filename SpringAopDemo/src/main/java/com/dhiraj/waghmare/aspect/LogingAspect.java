7f5c7c30cdf75e800c98e9c9c34e51c7e61623bb
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-03-11 21:57:10 +0530

    Aspect Oriented Programming

--- SpringAopDemo/src/main/java/com/dhiraj/waghmare/aspect/LogingAspect.java
+++ SpringAopDemo/src/main/java/com/dhiraj/waghmare/aspect/LogingAspect.java
@@ -1,5 +1,7 @@
 package com.dhiraj.waghmare.aspect;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,18 @@ public class LogingAspect {
 	{
 		
 		System.out.println("Logging before method Execution.....");
+	}
+	@After("execution(* com.dhiraj.waghmare.service.*.*(..))")
+	public void logAfter()
+	{
+		
+		System.out.println(" After method Execution.....");
+	}
+	@Around("execution(* com.dhiraj.waghmare.service.*.*(..))")
+	public void logAround()
+	{
+		
+		System.out.println("Around is fore Before and After");
 	}
 
 }
